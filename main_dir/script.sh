a271cc92fd5508aa0c59f027ffb9cb713b40791b
MariaDash <122399261+MariaDash@users.noreply.github.com>
2023-04-20 21:50:38 +0300

    Update script.sh

--- main_dir/script.sh
+++ main_dir/script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-cd dir1
-pwd
-mkdir folder1 folder2 folder3
-ls -ltr
-cd folder1
-pwd
-touch note1.txt note2.txt note3.txt j1.json j2.json
-ls -ltr
-mkdir newd1 newd2 newd3
-ls -ltr
-pwd
-mv note1.txt j1.json /d/Testing_Course/Git/main_dir
-ls
-cd /d/Testing_Course/Git/main_dir
-pwd 
-ls -ltr
-echo "All done!"
+&& cd dir1
+&& pwd
+&& mkdir folder1 folder2 folder3
+&& ls -ltr
+&& cd folder1
+&& pwd
+&& touch note1.txt note2.txt note3.txt j1.json j2.json
+&& ls -ltr
+&& mkdir newd1 newd2 newd3
+&& ls -ltr
+&& pwd
+&& mv note1.txt j1.json /d/Testing_Course/Git/main_dir
+&& ls
+&& cd /d/Testing_Course/Git/main_dir
+&& pwd 
+&& ls -ltr
+&& echo "All done!"

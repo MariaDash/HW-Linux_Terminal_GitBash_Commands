ee8989df7bebff0d555e014dcb6221a47bd6d7d8
MariaDash <122399261+MariaDash@users.noreply.github.com>
2023-04-21 14:20:46 +0300

    Update script.sh

--- main_dir/script.sh
+++ main_dir/script.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 FOLDER="dir1"
 
-rm -rf $FOLDER
-echo "rm -rf ${FOLDER}" \
-cd $FOLDER
-echo "cd ${FOLDER}" \
-mkdir folder1 folder2 folder3
-echo "mkdir folder1 folder2 folder3" \
-cd folder1
-echo "cd folder1" \
-
-
-touch note1.txt note2.txt note3.txt j1.json j2.json
-ls -ltr
-mkdir newd1 newd2 newd3
-ls -ltr
-pwd
-mv note1.txt j1.json /d/Testing_Course/Git/main_dir
-ls
-cd /d/Testing_Course/Git/main_dir
-pwd 
-ls -ltr
-echo "All done!"
+&& rm -rf $FOLDER                                            && echo "rm -rf ${FOLDER}" \                                      
+&& mkdir $FOLDER                                             && echo "mkdir $FOLDER" \  
+&& cd $FOLDER                                                && echo "cd ${FOLDER}" \                                       
+&& mkdir folder1 folder2 folder3                             && echo "mkdir folder1 folder2 folder3" \                                        
+&& cd folder1                                                && echo "cd folder1" \                                       
+&& touch note1.txt note2.txt note3.txt j1.json j2.json       && echo "touch note1.txt note2.txt note3.txt j1.json j2.json" \                                      
+&& mkdir newd1 newd2 newd3                                   && echo "mkdir newd1 newd2 newd3" \
+&& ls -la                                                    && echo "ls -la" \
+&& mv note1.txt j1.json /d/Testing_Course/Git/main_dir       && echo "mv note1.txt j1.json /d/Testing_Course/Git/main_dir" 
+&& echo "All done!"
